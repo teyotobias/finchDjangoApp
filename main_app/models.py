5563472ac976440b6743fc110608d2e351939948
teyotobias <teyotobias@utexas.edu>
2023-07-11 14:11:13 -0500

    Implemented class based views as well as CUD functionality

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.urls import reverse
+
 # Create your models here.
 
 
@@ -12,3 +14,6 @@ class Finch(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'finch_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 
 from .models import Finch
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 # Create your views here.
+
+class FinchCreate(CreateView):
+   model = Finch
+   fields = '__all__'
+
+class FinchUpdate(UpdateView):
+   model = Finch
+   fields = ['origin', 'description', 'lifespan']
+
+class FinchDelete(DeleteView):
+   model = Finch
+   success_url = '/finches'
+
 def home(request):
     return render(request, 'home.html')
 
@@ -14,9 +27,7 @@ def finches_index(request):
   finches = Finch.objects.all()
   return render(request, 'finches/index.html',{
      'finches': finches
-  }
-  )
-
+  })
 
 def finches_detail(request, finch_id):
    requestedfinch = Finch.objects.get(id=finch_id)
